e8720c363b13b56784f5a560eb052fa3756ae609
imbrainlezz <imbrainlezz@gmail.com>
2023-02-14 10:17:04 +0100

    Update StudentData.js
    CustomToolbar átszervezése StudentData komponensbe.
    Az Excel adatok exportálása már csak a kiválasztott sorokkal lehetséges.

--- vizsgaremek_palyakovetes/frontend/src/components/StudentData.js
+++ vizsgaremek_palyakovetes/frontend/src/components/StudentData.js
@@ -15,29 +15,29 @@ import {
 import { Button } from "@mui/material";
 import ExportExcel from "./ExportExcel";
 
-const columns = [
-  { field: "id", headerName: "ID", width: 70 },
-  { field: "firstName", headerName: "First name", width: 130 },
-  { field: "lastName", headerName: "Last name", width: 130 },
-  {
-    field: "age",
-    headerName: "Age",
-    type: "number",
-    width: 90,
-  },
-  {
-    field: "fullName",
-    headerName: "Full name",
-    description: "This column has a value getter and is not sortable.",
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ""} ${params.row.lastName || ""}`,
-  },
-];
+const data = {
 
-const rows = [
-  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
+  columns: [
+    { field: "id", headerName: "ID", width: 70 },
+    { field: "firstName", headerName: "First name", width: 130 },
+    { field: "lastName", headerName: "Last name", width: 130 },
+    {
+      field: "age",
+      headerName: "Age",
+      type: "number",
+      width: 90,
+    },
+    {
+      field: "fullName",
+      headerName: "Full name",
+      description: "This column has a value getter and is not sortable.",
+      sortable: false,
+      width: 160,
+      valueGetter: (params) =>
+        `${params.row.firstName || ""} ${params.row.lastName || ""}`,
+    },
+  ],
+  rows: [{ id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
   { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
   { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
   { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
@@ -55,41 +55,52 @@ const rows = [
   { id: 16, lastName: "Clifford", firstName: "Ferrara", age: 44 },
   { id: 17, lastName: "Frances", firstName: "Rossini", age: 36 },
   { id: 18, lastName: "Roxie", firstName: "Harvey", age: 65 },
-];
-
-function CustomToolbar() {
-  const date = new Date();
-
-  return (
-    <GridToolbarContainer>
-      <GridToolbarExportContainer>
-        <ExportExcel
-          excelData={rows}
-          fileName={`Diák adatok
-          ${date.getFullYear()}_${
-            date.getMonth() + 1
-          }.${date.getDate()}. ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`}
-        />
-
-        <GridCsvExportMenuItem />
-        <GridPrintExportMenuItem />
-      </GridToolbarExportContainer>
-    </GridToolbarContainer>
-  );
+]
 }
 
 export default function StudentData() {
+  const [selectedRows, setSelectedRows] = React.useState([]);
+
+  function CustomToolbar() {
+    const date = new Date();
+  
+    return (
+      <GridToolbarContainer>
+        <GridToolbarExportContainer>
+          <ExportExcel
+            excelData={selectedRows}
+            fileName={`Diák adatok ${date.getFullYear()}.${date.getMonth() + 1}.${date.getDate()}. ${date.getHours()}-${date.getMinutes()}-${date.getSeconds()}`}
+          />
+  
+          <GridCsvExportMenuItem />
+          <GridPrintExportMenuItem />
+        </GridToolbarExportContainer>
+      </GridToolbarContainer>
+    );
+  }
+
   return (
     <div style={{ height: 800, width: "90%", margin: "1rem auto" }}>
       <DataGrid
-        rows={rows}
-        columns={columns}
         pageSize={12}
         rowsPerPageOptions={[12]}
         checkboxSelection
+        columns={data.columns}
+        onSelectionModelChange={(ids) => {
+          const selectedIDs = new Set(ids);
+          const selectedRowData = data.rows.filter((row) => {
+            return selectedIDs.has(row.id)
+          }
+          );
+
+          setSelectedRows(selectedRowData);
+          console.log(selectedRows)
+          console.log(selectedRowData);
+        }}
         components={{
           Toolbar: CustomToolbar,
         }}
+        {...data}
       />
     </div>
   );
