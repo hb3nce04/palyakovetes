13fff3f9eac29e56d8e429f113e691394ef1bbc1
imbrainlezz <imbrainlezz@gmail.com>
2023-02-19 16:51:53 +0100

    további adatok feltöltése, adatbázis módosítása

--- vizsgaremek_palyakovetes/db/palyakovetes.sql
+++ vizsgaremek_palyakovetes/db/palyakovetes.sql
@@ -2,10 +2,10 @@
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Feb 19, 2023 at 04:06 PM
--- Server version: 10.4.21-MariaDB
--- PHP Version: 8.0.10
+-- Gép: 127.0.0.1
+-- Létrehozás ideje: 2023. Feb 19. 16:50
+-- Kiszolgáló verziója: 10.4.22-MariaDB
+-- PHP verzió: 8.1.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `palyakovetes`
+-- Adatbázis: `palyakovetes`
 --
 CREATE DATABASE IF NOT EXISTS `palyakovetes` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_hungarian_ci;
 USE `palyakovetes`;
@@ -26,7 +26,7 @@ USE `palyakovetes`;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `agazat`
+-- Tábla szerkezet ehhez a táblához `agazat`
 --
 
 CREATE TABLE `agazat` (
@@ -36,7 +36,7 @@ CREATE TABLE `agazat` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
 
 --
--- Dumping data for table `agazat`
+-- A tábla adatainak kiíratása `agazat`
 --
 
 INSERT INTO `agazat` (`id`, `nev`, `szam`) VALUES
@@ -106,20 +106,27 @@ INSERT INTO `agazat` (`id`, `nev`, `szam`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `felhasznalo`
+-- Tábla szerkezet ehhez a táblához `felhasznalo`
 --
 
 CREATE TABLE `felhasznalo` (
-  `om_azon` int(11) NOT NULL,
-  `iskolaid` int(2) NOT NULL,
+  `om_azon` bigint(11) NOT NULL,
   `jelszo` varchar(60) COLLATE utf8mb4_hungarian_ci NOT NULL,
   `admin` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
 
+--
+-- A tábla adatainak kiíratása `felhasznalo`
+--
+
+INSERT INTO `felhasznalo` (`om_azon`, `jelszo`, `admin`) VALUES
+(11111111111, '$2a$12$jSE6tVSj5JTIZxBgN7wpKeHn7oqUVBim0zO/R3iCA5HBvjgM7u5m6', 1),
+(22222222222, '$2a$12$7eWjhG.OcKNAeYR3NZVOg.kYEUtWsXKBWcnUGgbXlkM57EbEorqku', 0);
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `iskola`
+-- Tábla szerkezet ehhez a táblához `iskola`
 --
 
 CREATE TABLE `iskola` (
@@ -127,10 +134,26 @@ CREATE TABLE `iskola` (
   `nev` varchar(255) COLLATE utf8mb4_hungarian_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
 
+--
+-- A tábla adatainak kiíratása `iskola`
+--
+
+INSERT INTO `iskola` (`id`, `nev`) VALUES
+(1, 'Nyíregyházi SZC Bánki Donát Műszaki Technikum és Kollégium'),
+(2, 'Nyíregyházi SZC Bencs László Szakképző Iskola'),
+(3, 'Nyíregyházi SZC Inczédy György Szakképző Iskola és Kollégium'),
+(4, 'Nyíregyházi SZC Sipkay Barna Technikum'),
+(5, 'Nyíregyházi SZC Széchenyi István Technikum és Kollégium'),
+(6, 'Nyíregyházi SZC Teleki Blanka Szakképző Iskola és Kollégium'),
+(7, 'Nyíregyházi SZC Tiszavasvári Szakképző Iskola és Kollégium'),
+(8, 'Nyíregyházi SZC Vásárhelyi Pál Technikum'),
+(9, 'Nyíregyházi SZC Wesselényi Miklós Technikum és Kollégium'),
+(10, 'Nyíregyházi SZC Zay Anna Technikum és Kollégium');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `kategoria`
+-- Tábla szerkezet ehhez a táblához `kategoria`
 --
 
 CREATE TABLE `kategoria` (
@@ -139,7 +162,7 @@ CREATE TABLE `kategoria` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
 
 --
--- Dumping data for table `kategoria`
+-- A tábla adatainak kiíratása `kategoria`
 --
 
 INSERT INTO `kategoria` (`id`, `megnevezes`) VALUES
@@ -160,7 +183,7 @@ INSERT INTO `kategoria` (`id`, `megnevezes`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `osztaly`
+-- Tábla szerkezet ehhez a táblához `osztaly`
 --
 
 CREATE TABLE `osztaly` (
@@ -174,7 +197,7 @@ CREATE TABLE `osztaly` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `palya`
+-- Tábla szerkezet ehhez a táblához `palya`
 --
 
 CREATE TABLE `palya` (
@@ -187,7 +210,7 @@ CREATE TABLE `palya` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `szakma`
+-- Tábla szerkezet ehhez a táblához `szakma`
 --
 
 CREATE TABLE `szakma` (
@@ -196,10 +219,62 @@ CREATE TABLE `szakma` (
   `szam` varchar(255) COLLATE utf8mb4_hungarian_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
 
+--
+-- A tábla adatainak kiíratása `szakma`
+--
+
+INSERT INTO `szakma` (`id`, `nev`, `szam`) VALUES
+(1, 'Ács', '3458201'),
+(2, 'Asztalos', '3454302'),
+(3, 'Autóelektronikai műszerész', '5452501'),
+(4, 'Autószerelő', '5452502'),
+(5, 'Burkoló', '3458213'),
+(6, 'Cukrász', '3481101'),
+(7, 'Divat- és stílustervező', '5421102'),
+(8, 'Eladó', '3434101'),
+(9, 'Elektronikai technikus', '5452302'),
+(10, 'Épület- és szerkezetlakatos', '3458203'),
+(11, 'Faipari technikus', '5454301'),
+(12, 'Festő, mázoló, tapétázó', '3458204'),
+(13, 'Fodrász', '5481501'),
+(14, 'Gépgyártástechnológiai technikus', '5452103'),
+(15, 'Gépi forgácsoló', '3452103'),
+(16, 'Gyakorló ápoló', '5472302'),
+(17, 'Hegesztő', '3452106'),
+(18, 'Informatikai rendszerüzemeltető', '5448106'),
+(19, 'Ipari gépész', '3452104'),
+(20, 'Ipari gumitermék előállító', '3454304'),
+(21, 'Irodai titkár', '5434603'),
+(22, 'Járműfényező', '3452503'),
+(23, 'Karosszérialakatos', '3452506'),
+(24, 'Kereskedő', '5434101'),
+(25, 'Kisgyermekgondozó, -nevelő', '5476102'),
+(26, 'Kozmetikus', '5481502'),
+(27, 'Kőműves', '3458214'),
+(28, 'Környezetvédelmi technikus', '5485001'),
+(29, 'Központifűtés- és gázhálózat rendszerszerelő', '3458209'),
+(30, 'Logisztikai és szállítmányozási ügyintéző', '5484111'),
+(31, 'Magasépítő technikus', '5458203'),
+(32, 'Mechatronikai technikus', '5452304'),
+(33, 'Mélyépítő technikus', '5458204'),
+(34, 'Műszaki informatikus', '5448105'),
+(35, 'Női szabó', '3454206'),
+(36, 'Pénzügyi-számviteli ügyintéző', '5434401'),
+(37, 'Pincér', '3481103'),
+(38, 'Sportedző', '5481302'),
+(39, 'Szakács', '3481104'),
+(40, 'Számítógép-szerelő, karbantartó', '3452302'),
+(41, 'Szociális gondozó és ápoló', '3476201'),
+(42, 'Turisztikai szervező, értékesítő', '5481203'),
+(43, 'Vendéglátásszervező', '5481101'),
+(44, 'Villanyszerelő', '3452204'),
+(45, 'Víz-, csatorna- és közmű- rendszerszerelő', '3458212'),
+(46, 'Vízgazdálkodó technikus', '5485302');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tanulo`
+-- Tábla szerkezet ehhez a táblához `tanulo`
 --
 
 CREATE TABLE `tanulo` (
@@ -212,39 +287,38 @@ CREATE TABLE `tanulo` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
 
 --
--- Indexes for dumped tables
+-- Indexek a kiírt táblákhoz
 --
 
 --
--- Indexes for table `agazat`
+-- A tábla indexei `agazat`
 --
 ALTER TABLE `agazat`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `szam` (`szam`);
 
 --
--- Indexes for table `felhasznalo`
+-- A tábla indexei `felhasznalo`
 --
 ALTER TABLE `felhasznalo`
   ADD PRIMARY KEY (`om_azon`),
-  ADD UNIQUE KEY `jelszo` (`jelszo`),
-  ADD UNIQUE KEY `iskolaid` (`iskolaid`);
+  ADD UNIQUE KEY `jelszo` (`jelszo`);
 
 --
--- Indexes for table `iskola`
+-- A tábla indexei `iskola`
 --
 ALTER TABLE `iskola`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `kategoria`
+-- A tábla indexei `kategoria`
 --
 ALTER TABLE `kategoria`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `megnevezes` (`megnevezes`);
 
 --
--- Indexes for table `osztaly`
+-- A tábla indexei `osztaly`
 --
 ALTER TABLE `osztaly`
   ADD PRIMARY KEY (`id`),
@@ -252,7 +326,7 @@ ALTER TABLE `osztaly`
   ADD KEY `osztaly_ibfk_3` (`felhasznalo_om`);
 
 --
--- Indexes for table `palya`
+-- A tábla indexei `palya`
 --
 ALTER TABLE `palya`
   ADD PRIMARY KEY (`id`),
@@ -260,14 +334,14 @@ ALTER TABLE `palya`
   ADD KEY `palya_ibfk_3` (`kategoriaid`);
 
 --
--- Indexes for table `szakma`
+-- A tábla indexei `szakma`
 --
 ALTER TABLE `szakma`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `szam` (`szam`);
 
 --
--- Indexes for table `tanulo`
+-- A tábla indexei `tanulo`
 --
 ALTER TABLE `tanulo`
   ADD PRIMARY KEY (`om_azon`),
@@ -276,70 +350,44 @@ ALTER TABLE `tanulo`
   ADD KEY `tanulo_ibfk_7` (`szakid`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- A kiírt táblák AUTO_INCREMENT értéke
 --
 
 --
--- AUTO_INCREMENT for table `agazat`
+-- AUTO_INCREMENT a táblához `agazat`
 --
 ALTER TABLE `agazat`
   MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
 
 --
--- AUTO_INCREMENT for table `iskola`
+-- AUTO_INCREMENT a táblához `iskola`
 --
 ALTER TABLE `iskola`
-  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
--- AUTO_INCREMENT for table `kategoria`
+-- AUTO_INCREMENT a táblához `kategoria`
 --
 ALTER TABLE `kategoria`
   MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
--- AUTO_INCREMENT for table `osztaly`
+-- AUTO_INCREMENT a táblához `osztaly`
 --
 ALTER TABLE `osztaly`
   MODIFY `id` int(4) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `palya`
+-- AUTO_INCREMENT a táblához `palya`
 --
 ALTER TABLE `palya`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `szakma`
+-- AUTO_INCREMENT a táblához `szakma`
 --
 ALTER TABLE `szakma`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `osztaly`
---
-ALTER TABLE `osztaly`
-  ADD CONSTRAINT `osztaly_ibfk_2` FOREIGN KEY (`iskolaid`) REFERENCES `iskola` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `osztaly_ibfk_3` FOREIGN KEY (`felhasznalo_om`) REFERENCES `felhasznalo` (`om_azon`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `palya`
---
-ALTER TABLE `palya`
-  ADD CONSTRAINT `palya_ibfk_2` FOREIGN KEY (`diak_om_azon`) REFERENCES `tanulo` (`om_azon`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `palya_ibfk_3` FOREIGN KEY (`kategoriaid`) REFERENCES `kategoria` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `tanulo`
---
-ALTER TABLE `tanulo`
-  ADD CONSTRAINT `tanulo_ibfk_5` FOREIGN KEY (`osztalyid`) REFERENCES `osztaly` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `tanulo_ibfk_6` FOREIGN KEY (`agazatid`) REFERENCES `agazat` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `tanulo_ibfk_7` FOREIGN KEY (`szakid`) REFERENCES `szakma` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
