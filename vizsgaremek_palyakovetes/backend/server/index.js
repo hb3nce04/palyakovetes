05f38fd1c7d84610e24dbc1aade964c5927c7822
Labbancz Balázs <balazs.labbancz04@gmail.com>
2023-02-25 17:17:14 +0100

    login folytatás

--- vizsgaremek_palyakovetes/backend/server/index.js
+++ vizsgaremek_palyakovetes/backend/server/index.js
@@ -23,15 +23,41 @@ connection.connect((err) => {
 });
 
 /*
-app.get("/login", (req, res)=> {
-    connection.query("SELECT * FROM user WHERE email = ?;", [email], (err, data) => {
-        if (err) res.status(500).send(err);
-        if(data.length === 0) res.status(204).send("User not found.");
+app.post("/register", (req, res) => {
+  const { om_azon, jelszo } = req.body;
+  connection.query(
+    "SELECT * FROM felhasznalo WHERE om_azon LIKE ?;",
+    [om_azon],
+    (err, rows) => {
+      if (err) res.status(500).json("Error");
+      if (rows.length) res.status(409).json("User already exists!");
 
-        const isCorrectPassword = bcrypt.compareSync(req.body.password, data[0].password);
+      const salt = bcrypt.genSaltSync(12);
+      const hash = bcrypt.hashSync(jelszo, salt);
+
+      connection.query(
+        "INSERT INTO felhasznalo (om_azon, jelszo) VALUES (?);",
+        [[om_azon, hash]],
+        (err, data) => {
+          if (err) res.status(500).json(err);
+          res.status(201).json("User has been created.");
+        }
+      );
+    }
+  );
+});
+*/
+app.get("/login", (req, res)=> {
+  const { om_azon, jelszo } = req.body;
+    connection.query("SELECT * FROM felhasznalo WHERE om_azon = ?;", [om_azon], (err, data) => {
+        if(data.length === 0){
+          res.status(204).send("User not found.");
+        }
+
+        const isCorrectPassword = bcrypt.compareSync(jelszo, data[0].jelszo);
         
         if(!isCorrectPassword)
-            res.status(400).send("Wrong username or password.");
+            res.status(400).json({message: "Wrong username or password."});
 
 
         const token = jwt.sign({id: data[0].id}, "secret");
@@ -41,7 +67,7 @@ app.get("/login", (req, res)=> {
         }).status(200).json({isAdmin: data[0].admin});
     });
 });
-*/
+
 
 app.listen(8080, () => {
   console.log("Listening to port 8080");
