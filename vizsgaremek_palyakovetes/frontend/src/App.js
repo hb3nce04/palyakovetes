581fdc3a3fb5322b14bb3712c298fa6117c0f7cc
Labbancz Balázs <balazs.labbancz04@gmail.com>
2023-03-17 11:11:06 +0100

    Context javítások

--- vizsgaremek_palyakovetes/frontend/src/App.js
+++ vizsgaremek_palyakovetes/frontend/src/App.js
@@ -9,31 +9,15 @@ import { DarkMode } from "./components/DarkMode";
 import { AddNewStudent } from "./pages/AddNewStudent";
 import { UpdateStudent } from "./pages/UpdateStudent";
 import { ClassChooser } from "./pages/ClassChooser";
-<<<<<<< Updated upstream
+
 import { Contact } from "./pages/Contact";
-=======
+
 import { Auth } from "./context/auth/AuthContext";
->>>>>>> Stashed changes
+
 
 function App() {
   return (
     <div className="App">
-<<<<<<< Updated upstream
-      
-      <BrowserRouter>
-        <DarkMode>
-          <Routes>
-          <Route path="*"  element={<GenericNotFound />} />
-            <Route path="/">
-              <Route path = "/" index element={<Login />} />
-              <Route path="contact" element={<Contact />} />
-              <Route path="home" element={<Home />} />
-              <Route path = "classchooser" element={<ClassChooser />} />
-              <Route path="student">
-                <Route path="add" element={<AddNewStudent/>}></Route>
-                <Route path=":studentId">
-                  <Route path="update" element={<UpdateStudent/>}></Route>
-=======
       <Auth>
         <BrowserRouter>
           <DarkMode>
@@ -41,13 +25,13 @@ function App() {
               <Route path="*" element={<GenericNotFound />} />
               <Route path="/">
                 <Route path="/" index element={<Login />} />
+                <Route path="contact" element={<Contact />} />
                 <Route path="home" element={<Home />} />
                 <Route path="classchooser" element={<ClassChooser />} />
                 <Route path="student">
                   <Route path="add" element={<AddNewStudent />}></Route>
                   <Route path=":studentId">
                     <Route path="update" element={<UpdateStudent />}></Route>
->>>>>>> Stashed changes
                   </Route>
                 </Route>
               </Route>
@@ -60,3 +44,4 @@ function App() {
 }
 
 export default App;
+
