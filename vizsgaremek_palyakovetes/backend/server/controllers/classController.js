973642e45bbed68a431e5553dc6e84668556e2d4
Labbancz Balázs <balazs.labbancz04@gmail.com>
2023-04-02 14:33:56 +0200

    Osztály hozzáadás

--- vizsgaremek_palyakovetes/backend/server/controllers/classController.js
+++ vizsgaremek_palyakovetes/backend/server/controllers/classController.js
@@ -1,15 +1,9 @@
-import {
-  db
-} from "../db.js";
-import {
-  StatusCodes
-} from "http-status-codes";
+import { db } from "../db.js";
+import { StatusCodes } from "http-status-codes";
 
 export const getClasses = (req, res) => {
   console.log(req.body);
-  const {
-    om_azon
-  } = req.body;
+  const { om_azon } = req.body;
 
   if (!om_azon) {
     return res.status(StatusCodes.UNAUTHORIZED).send("Missing OM ID");
@@ -43,3 +37,30 @@ export const getClasses = (req, res) => {
     );
   }
 };
+
+export const createClass = (req, res) =>{
+  const {iskolaid, felhasznalo_om, nev, vegzesi_ev} = req.body;
+  db.query(
+    "SELECT * FROM osztaly WHERE iskolaid = ? AND nev = ? AND vegzesi_ev = ?",
+    [iskolaid, nev, vegzesi_ev],
+    (err, data) => {
+      if(err){
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send("Error: "+err);
+      }
+      if(data.length !== 0){
+        return res.status(StatusCodes.BAD_REQUEST).send("Class already exists");
+      }else{
+        db.query(
+          "INSERT INTO osztaly (iskolaid, felhasznalo_om, nev, vegzesi_ev) VALUES (?,?,?,?)",
+          [iskolaid, felhasznalo_om, nev, vegzesi_ev],
+          (err, data) =>{
+            if(err){
+              return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send("Error: "+err)
+            }
+            return res.status(StatusCodes.OK).json(data);
+          }
+        );
+      }
+    }
+  );
+}
--- vizsgaremek_palyakovetes/backend/server/routes/classRoute.js
+++ vizsgaremek_palyakovetes/backend/server/routes/classRoute.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getClasses } from "../controllers/classController.js";
+import { getClasses, createClass } from "../controllers/classController.js";
 
 const router = express.Router();
 
 router.post("/class_chooser", getClasses);
+router.post("/create", createClass);
 
 export default router;
