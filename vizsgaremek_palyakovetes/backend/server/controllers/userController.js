7a201e5a7d0e6d81a8d553a2469de09ebc06c982
Labbancz Balázs <balazs.labbancz04@gmail.com>
2023-04-14 23:44:47 +0200

    Felhasználói jelszó módosítás backenden

--- vizsgaremek_palyakovetes/backend/server/controllers/userController.js
+++ vizsgaremek_palyakovetes/backend/server/controllers/userController.js
@@ -1,5 +1,6 @@
 import { db } from "../db.js";
 import { StatusCodes } from "http-status-codes";
+import bcrypt from "bcryptjs";
 
 export const getUsers = (req, res) => {
   db.query("SELECT * FROM felhasznalo;", (err, data) => {
@@ -42,6 +43,31 @@ export const deleteUser = (req, res) => {
               return res.status(StatusCodes.OK).send("user has been deleted");
             }
           );
+        }
+      }
+    );
+  }
+};
+
+export const updatePassword = (req, res) => {
+  const { om_azon, jelszo } = req.body;
+  if (!om_azon || !jelszo) {
+    return res.status(StatusCodes.BAD_REQUEST).send("Missing parameters");
+  } else if (jelszo.length < 8) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .send("Password must be at least 8 characters long");
+  } else {
+    const salt = bcrypt.genSaltSync(12);
+    const hash = bcrypt.hashSync(jelszo, salt);
+    db.query(
+      "UPDATE felhasznalo SET jelszo = ? WHERE om_azon = ?;",
+      [hash, om_azon],
+      (err, data) => {
+        if (err) {
+          return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(err);
+        } else {
+          return res.status(StatusCodes.OK).send("Password has been modified");
         }
       }
     );
--- vizsgaremek_palyakovetes/backend/server/routes/user.js
+++ vizsgaremek_palyakovetes/backend/server/routes/user.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getUsers, deleteUser } from "../controllers/userController.js";
+import { getUsers, deleteUser, updatePassword } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.get("/userList", getUsers);
 router.post("/deleteUser", deleteUser);
+router.post("/updatePassword", updatePassword);
 
 export default router;
