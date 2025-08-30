78878d4fff9456904b0814203cd7422daf41b5c6
hb3nce04 <60313402+hb3nce04@users.noreply.github.com>
2025-08-30 20:43:20 +0200

    feat: add sector_id to students seed

--- project/backend/prisma/seed.js
+++ project/backend/prisma/seed.js
@@ -45,10 +45,10 @@ async function seedingOthers() {
 	}).then(() => console.log("Classes seeded..."));
     await prisma.Student.createMany({
 		data: [
-            {id: 12345678910, name: "Kovács József", class_id: 2, day_shift: true, profession_id: 12},
-            {id: 12345678911, name: "Soós Gizella", class_id: 2, day_shift: false, profession_id: 13},
-            {id: 12345678912, name: "Lukács Donát", class_id: 2, day_shift: true, profession_id: 20},
-        ]
+            {id: 12345678910, name: "Kovács József", class_id: 2, day_shift: true, profession_id: 12, sector_id: 1},
+            {id: 12345678911, name: "Soós Gizella", class_id: 2, day_shift: false, profession_id: 13, sector_id: 1},
+            {id: 12345678912, name: "Lukács Donát", class_id: 2, day_shift: true, profession_id: 20, sector_id: 1},
+        ]0
 	}).then(() => console.log("Students seeded..."));
 
 }
