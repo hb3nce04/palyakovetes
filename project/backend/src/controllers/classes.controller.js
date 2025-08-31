bbb019b6abc56fd37c25128b8186b8b967077e27
hb3nce04 <60313402+hb3nce04@users.noreply.github.com>
2025-08-31 10:50:49 +0200

    feat: update class

--- project/backend/src/controllers/classes.controller.js
+++ project/backend/src/controllers/classes.controller.js
@@ -1,91 +1,129 @@
-import { prisma } from "../utils/prisma-client.js";
-import { validationMessage } from "../middlewares/validation.middleware.js";
-import { StatusCodes, ReasonPhrases } from "http-status-codes";
+import {prisma} from "../utils/prisma-client.js";
+import {validationMessage} from "../middlewares/validation.middleware.js";
+import {StatusCodes, ReasonPhrases} from "http-status-codes";
 
 export const getClasses = (req, res) => {
-	prisma.Class.findMany({ include: { School: true } })
-		.then((classes) => {
-			return res.status(StatusCodes.OK).json(classes);
-		})
-		.catch((err) => {
-			return res
-				.status(StatusCodes.INTERNAL_SERVER_ERROR)
-				.send(ReasonPhrases.INTERNAL_SERVER_ERROR);
-		});
+    prisma.Class.findMany({include: {School: true}})
+        .then((classes) => {
+            return res.status(StatusCodes.OK).json(classes);
+        })
+        .catch((err) => {
+            return res
+                .status(StatusCodes.INTERNAL_SERVER_ERROR)
+                .send(ReasonPhrases.INTERNAL_SERVER_ERROR);
+        });
 };
 
 export const getStudentsByClassID = (req, res) => {
-	const { id } = req.params;
+    const {id} = req.params;
 
-	prisma.Student.findMany({
-		where: { class_id: parseInt(id) },
-		include: { Sector: true, Profession: true, Field: true }
-	})
-		.then((students) => {
-			return res.status(StatusCodes.OK).json(students);
-		})
-		.catch((err) => {
-			return res
-				.status(StatusCodes.INTERNAL_SERVER_ERROR)
-				.send(ReasonPhrases.INTERNAL_SERVER_ERROR);
-		});
+    prisma.Student.findMany({
+        where: {class_id: parseInt(id)},
+        include: {Sector: true, Profession: true, Field: true}
+    })
+        .then((students) => {
+            return res.status(StatusCodes.OK).json(students);
+        })
+        .catch((err) => {
+            return res
+                .status(StatusCodes.INTERNAL_SERVER_ERROR)
+                .send(ReasonPhrases.INTERNAL_SERVER_ERROR);
+        });
 };
 
 export const createClass = async (req, res) => {
-	const { schoolId, name, finishingYear } = req.body;
-	const userId = req.user.id;
+    const {schoolId, name, finishingYear} = req.body;
+    const userId = req.user.id;
 
-	const message = validationMessage(req);
-	if (message) {
-		return res.status(StatusCodes.BAD_REQUEST).json({ message });
-	}
+    const message = validationMessage(req);
+    if (message) {
+        return res.status(StatusCodes.BAD_REQUEST).json({message});
+    }
 
-	const foundClass = await prisma.Class.findFirst({
-		where: {
-			school_id: parseInt(schoolId),
-			name,
-			finishing_year: parseInt(finishingYear)
-		}
-	});
+    const foundClass = await prisma.Class.findFirst({
+        where: {
+            school_id: parseInt(schoolId),
+            name,
+            finishing_year: parseInt(finishingYear)
+        }
+    });
 
-	if (foundClass) {
-		return res
-			.status(StatusCodes.CONFLICT)
-			.json({ message: "Az osztály már létezik!" });
-	}
+    if (foundClass) {
+        return res
+            .status(StatusCodes.CONFLICT)
+            .json({message: "Az osztály már létezik!"});
+    }
 
-	const newClass = await prisma.Class.create({
-		data: {
-			School: { connect: { id: parseInt(schoolId) } },
-			User: { connect: { id: userId } },
-			name,
-			finishing_year: parseInt(finishingYear)
-		}
-	}).then((cls) => {
-		return res
-			.status(StatusCodes.CREATED)
-			.json({ message: "Az osztály sikeresen létrehozva." });
-	});
+    const newClass = await prisma.Class.create({
+        data: {
+            School: {connect: {id: parseInt(schoolId)}},
+            User: {connect: {id: userId}},
+            name,
+            finishing_year: parseInt(finishingYear)
+        }
+    }).then((cls) => {
+        return res
+            .status(StatusCodes.CREATED)
+            .json({message: "Az osztály sikeresen létrehozva."});
+    });
 };
 
 export const deleteClassByID = (req, res) => {
-	const { id } = req.params;
+    const {id} = req.params;
 
-	const deletedClass = prisma.Class.delete({
-		where: {
-			id: parseInt(id)
-		}
-	})
-		.then(() => {
-			return res
-				.status(StatusCodes.OK)
-				.json({ message: "Az osztály sikeresen törölve." });
-		})
-		.catch((err) => {
-			if (err.code === "P2025") {
-				return res.status(StatusCodes.NOT_FOUND).json({
-					message: "Az adott azonosítójú osztály nem létezik."
-				});
-			}
-		});
+    const deletedClass = prisma.Class.delete({
+        where: {
+            id: parseInt(id)
+        }
+    })
+        .then(() => {
+            return res
+                .status(StatusCodes.OK)
+                .json({message: "Az osztály sikeresen törölve."});
+        })
+        .catch((err) => {
+            if (err.code === "P2025") {
+                return res.status(StatusCodes.NOT_FOUND).json({
+                    message: "Az adott azonosítójú osztály nem létezik."
+                });
+            }
+        });
 };
+
+export const updateClassById = async (req, res) => {
+    const {id} = req.params;
+    const {schoolId, name, finishingYear} = req.body;
+
+    const message = validationMessage(req);
+    if (message) {
+        return res.status(StatusCodes.BAD_REQUEST).json({message});
+    }
+
+    const foundClass = await prisma.Class.findFirst({
+        where: {id: parseInt(id)}
+    });
+
+    if (!foundClass) {
+        return res
+            .status(StatusCodes.CONFLICT)
+            .json({message: "Az osztály nem létezik!"});
+    }
+
+    const updatedField = await prisma.Class.update({
+        where: {
+            id: parseInt(id)
+        },
+        data: {
+            School: {
+                connect: {
+                    id: parseInt(schoolId)
+                }
+            },
+            name, finishing_year: parseInt(finishingYear)
+        }
+    });
+
+    return res.status(StatusCodes.OK).json({
+        message: "Az osztály sikeresen frissítve."
+    });
+}
--- project/backend/src/routes/classes.route.js
+++ project/backend/src/routes/classes.route.js
@@ -3,7 +3,7 @@ import {
 	getClasses,
 	getStudentsByClassID,
 	createClass,
-	deleteClassByID
+	deleteClassByID, updateClassById
 } from "../controllers/classes.controller.js";
 
 import { classValidation } from "../middlewares/validation.middleware.js";
@@ -13,6 +13,7 @@ const router = Router();
 router.get("/", getClasses);
 router.post("/", classValidation, createClass);
 router.delete("/:id", deleteClassByID);
+router.put("/:id", classValidation, updateClassById);
 
 router.get("/:id/students", getStudentsByClassID);
 
